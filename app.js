598066740eaf885723452d6ae8946a48df92fe16
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-01-10 15:34:53 -0800

    Initializes and adds a table to sales.html instead of a list

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function addStore(store) {
   store.storeNumber = storesList.length;
 }
 
-// Define store methods and add them to the Store objects' prototype
+// Define checkSales method and add it to the Store objects' prototype
 Store.prototype.checkSales = function () {
   // Check each hour for how many cookies were sold
   for (var hour = this.timeOpening; hour < this.timeClosing + 1; hour++) {
@@ -39,6 +39,7 @@ Store.prototype.checkSales = function () {
   }
 };
 
+// Define randomCustomers helper method and add it to the Store objects' prototype
 Store.prototype.randomCustomers = function () {
   // Generate a random number of customers based on min & max for this store
   var result = Math.floor(Math.random() * (this.maxHourlyCustomers + this.minHourlyCustomers) + this.minHourlyCustomers);
@@ -57,11 +58,50 @@ for (var i = 1; i < 6; i++) {
   eval('addStore (store' + i + ');');
 }
 
+salesReportInit();
+
 // BEGIN GENERATING SALES REPORT
-// var dailyReport = document.getElementById('daily_sales');
-// console.log(dailyReport);
+function salesReportInit() {
+  // identify parent node on the DOM (HTML section "sales_report")
+  var dailyReport = document.getElementById('sales_report');
+  console.log(dailyReport);
+
+  // create a new table & append it
+  var elTable = document.createElement('table');
+  dailyReport.appendChild(elTable);
+
+  // create the header row and append it to the table just created, along with first blank table heading cell
+  var elTableRow = document.createElement('tr');
+  elTable.appendChild(elTableRow);
+  var elFirstTableHeading = document.createElement('th');
+  elFirstTableHeading.setAttribute('class', 'table_header');
+  elTableRow.appendChild(elFirstTableHeading);
+
+  // create table headings and add as the contents of the header row
+  // starting with 6 AM
+  for (var i = 6; i < 18; i++) {
+    var elTableHeading = document.createElement('th');
+    // set class of the table headings
+    elTableHeading.setAttribute('class', 'table_header');
+    // create a user-friendly time stamp
+    if (i < 12) {
+      var hourString = i + ':00 AM';
+    } else if (i === 12) {
+      var hourString = '12:00 PM';
+    } else {
+      var hourString = (i - 12) + ':00 PM';
+    }
+    // set text content to the hour
+    elTableHeading.textContent = hourString;
+    elTableRow.appendChild(elTableHeading);
+  }
+}
+// Create a new row for each store
+// for (var index = 5; index < 21; i++) {
+// }
 // for (var i = 0; i < storesList.length; i++) {
-//   storesList[i].checkCookieSales();
+//   // check sales for each store
+//   storesList[i].checkSales();
 //   // create a new <p> element
 //   var pElement = document.createElement('p');
 //   var storeID = 'store' + (i + 1);
@@ -84,10 +124,11 @@ for (var i = 1; i < 6; i++) {
 //     // create a new list item
 //     var listItemElement = document.createElement('li');
 //     // assign class attribute of "hourly_report" to the list item
-//     listItemElement.setAttribute('class', 'hourly_report');
+//
+//     // REDEFINE THIS
+//     // listItemElement.setAttribute('class', 'hourly_report');
 //     // set text content of the list item to content of cookiesSoldHourly for this location
-//     listItemElement.textContent = (storesList[i].storetimeOpening + j) + ':00 - ' + storesList[i].cookiesSoldHourly[j] + ' cookies sold';
-//     // listItemElement.textContent = (storesList[i].storetimeOpening + j) + ':00: ' + storesList[i].cookiesSoldHourly[j] + 'cookies storesList[i].cookiesSoldHourly[j]';
+//     listItemElement.textContent = (storesList[i].timeOpening + j) + ':00 - ' + storesList[i].cookiesSoldHourly[j] + ' cookies sold';
 //     // add the new list item as a child of the given store
 //     listForStore.appendChild(listItemElement);
 //   };
