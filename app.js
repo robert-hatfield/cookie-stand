630762121d955068746026f544e4175a741756f7
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-01-09 16:08:28 -0800

    Renamed property to distinguish from method

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ var store1 = {
   avgCookiesPerCustomer: 6.3,
   storeOpeningTime: 6,
   storeClosingTime: 20,
-  cookiesSoldHourly: [],
+  cookiesSoldToday: [],
   // Generate a random number of customers based on minimum & maximum customers per hour
   randomCustomers: function() {
     var result = Math.floor(Math.random() * (this.maxHourlyCustomers + this.minHourlyCustomers) + this.minHourlyCustomers);
     console.log('Customers this hour: ' + result);
     return result;
   },
-  hourlyCookieSales: function() {
+  checkCookieSales: function() {
     // Check each hour for how many cookies were sold
     for (var hour = this.storeOpeningTime; hour < (this.storeClosingTime + 1); hour++) {
-      console.log('Hours elapsed: ' + this.cookiesSoldHourly.length);
-      this.cookiesSoldHourly[hour - this.storeOpeningTime] = Math.ceil(this.randomCustomers() * this.avgCookiesPerCustomer); // use Math.ceil since nobody buys a fraction of a cookie
-      console.log('At ' + hour + ':00, ' + this.cookiesSoldHourly[hour - this.storeOpeningTime] + ' cookies were sold.');
-      console.log(this.cookiesSoldHourly);
+      console.log('Hours elapsed: ' + this.cookiesSoldToday.length);
+      this.cookiesSoldToday[hour - this.storeOpeningTime] = Math.ceil(this.randomCustomers() * this.avgCookiesPerCustomer); // use Math.ceil to round up, since nobody buys a fraction of a cookie
+      console.log('At ' + hour + ':00, ' + this.cookiesSoldToday[hour - this.storeOpeningTime] + ' cookies were sold.');
+      console.log(this.cookiesSoldToday);
     }
   }
 };
-store1.hourlyCookieSales();
+store1.checkCookieSales();
